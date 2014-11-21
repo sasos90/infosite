8bb4dc9a1af9c27a297a1d5f768cf812db031ae9
Saso Sabotin <sasos90@gmail.com>
2014-11-21 17:11:55 +0100

    First html layout loaded

---
+++ application/views/header.php
@@ -0,0 +1,7 @@
+<html>
+    <head>
+        <script src="style/js/lib/jquery-1.11.1.min.js"></script>
+        <script src="style/js/script.js"></script>
+    </head>
+    <body>
+        First layout
