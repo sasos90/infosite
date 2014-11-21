8c9e08c9c8a6e964e2348f6a14dfdf9eb7c49cd1
Saso Sabotin <sasos90@gmail.com>
2014-11-21 16:31:38 +0100

    Grunt file and package.json modified

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,26 @@
 module.exports = function(grunt) {
+    require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         compass: {
-            dist: {
+            dev: {
                 options: {
                     sassDir: 'style/sass',
                     cssDir: 'style/css',
                     force: true,
-                    relativeAssets : true
+                    relativeAssets     : true,
+                    require            : ['singularitygs', 'breakpoint'],
+                    environment: 'development',
+                    outputStyle: 'expanded'
                 }
             }
         },
         watch: {
             css: {
-                files: '**/*.scss',
-                tasks: ['compass']
+                files: ['**/*.scss'],
+                tasks: ['compass:dev']
             }
         }
     });
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-compass');
-    grunt.registerTask('default',['compass:dist']);
+    grunt.registerTask('default',['compass:dev']);
 }
