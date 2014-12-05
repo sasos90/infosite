f1c9c4e3878c833fc6af9db04d6937d8fe8ac9b7
Saso Sabotin <sasos90@gmail.com>
2014-12-05 14:08:34 +0100

    Design changed

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,2 +1,5 @@
+    <script src="style/js/lib/jquery-1.11.1.min.js"></script>
+    <script src="style/js/script.js"></script>
+    <script src="style/js/lib/bootstrap.min.js"></script>
     </body>
 </html>
