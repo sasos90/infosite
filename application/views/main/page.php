fe7a1188c4e78fe88e95cd00f76bed27959e7bd0
Saso Sabotin <sasos90@gmail.com>
2014-12-08 14:14:10 +0100

    Loading sections in first page

--- application/views/main/page.php
+++ application/views/main/page.php
@@ -1,38 +1,6 @@
 <section class="container-fluid" id="section1">
-    <h1 class="text-center v-center">Sašo Sabotin</h1>
-    <h2 class="text-center">Personal info page</h2>
-    <div class="container">
-        <div class="page-header" id="banner">
-        </div>
-        <div class="alert alert-dismissable alert-warning">
-            <h4>Warning!</h4>
-            <p>The page is under construction.</p>
-        </div>
-        <div class="progress">
-            <div class="progress-bar progress-bar-success" style="width: 65%"></div>
-        </div>
-    </div>
+    <?php $this->load->view("my_controller/index/sections/section1"); ?>
 </section>
 <section class="container-fluid" id="section2">
-    <h1 class="text-center v-center">Sectionalize.</h1>
-    <div class="container">
-      <div class="row">
-          <div class="col-sm-4">
-            <div class="row">
-              <div class="col-sm-10 col-sm-offset-2 text-center"><h3>Robust</h3><p>There is other content and snippets of details or features that can be placed here..</p><i class="fa fa-cog fa-5x"></i></div>
-            </div>
-          </div>
-          <div class="col-sm-4 text-center">
-            <div class="row">
-              <div class="col-sm-10 col-sm-offset-1 text-center"><h3>Simple</h3><p>You may also want to create content that compells users to scroll down more..</p><i class="fa fa-user fa-5x"></i></div>
-            </div>
-          </div>
-          <div class="col-sm-4 text-center">
-            <div class="row">
-              <div class="col-sm-10 text-center"><h3>Clean</h3><p>In the first 30 seconds of a user's visit to your site they decide if they're going to stay..</p><i class="fa fa-mobile fa-5x"></i></div>
-            </div>
-          </div>
-      </div><!--/row-->
-    <div class="row"><br></div>
-  </div><!--/container-->
+    <?php $this->load->view("my_controller/index/sections/section2"); ?>
 </section>
---
+++ application/views/my_controller/index/sections/section1.php
@@ -0,0 +1,13 @@
+<h1 class="text-center v-center">Sašo Sabotin</h1>
+<h2 class="text-center">Personal info page</h2>
+<div class="container">
+    <div class="page-header" id="banner">
+    </div>
+    <div class="alert alert-dismissable alert-warning">
+        <h4>Warning!</h4>
+        <p>The page is under construction.</p>
+    </div>
+    <div class="progress">
+        <div class="progress-bar progress-bar-success" style="width: 65%"></div>
+    </div>
+</div>
