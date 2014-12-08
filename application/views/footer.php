eed80e2d219c9da8c0929b1dd88d36326b756ec2
Saso Sabotin <sasos90@gmail.com>
2014-12-08 14:16:21 +0100

    Loading script.js

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,4 +1,5 @@
     <script src="style/js/libs/jquery-1.11.1.min.js"></script>
     <script src="style/libs/bootstrap-3.1.1/dist/js/bootstrap.min.js"></script>
+    <script src="style/js/script.js"></script>
     </body>
 </html>
