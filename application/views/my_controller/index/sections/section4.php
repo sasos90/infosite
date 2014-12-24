f62509d29593da9d717fe69a223245df594ee72f
Saso Sabotin <sasos90@gmail.com>
2014-12-24 16:13:42 +0100

    Section 3 done

--- application/views/my_controller/index/sections/section4.php
+++ application/views/my_controller/index/sections/section4.php
@@ -1,21 +1,39 @@
-<h1 class="text-center v-center">Sectionalize4.</h1>
+<h1 class="text-center v-center">Skills</h1>
 <div class="container">
-    <div class="row">
-        <div class="col-sm-4">
-            <div class="row">
-                <div class="col-sm-10 col-sm-offset-2 text-center"><h3>Robust</h3><p>There is other content and snippets of details or features that can be placed here..</p><i class="fa fa-cog fa-5x"></i></div>
-            </div>
-        </div>
-        <div class="col-sm-4 text-center">
-            <div class="row">
-                <div class="col-sm-10 col-sm-offset-1 text-center"><h3>Simple</h3><p>You may also want to create content that compells users to scroll down more..</p><i class="fa fa-user fa-5x"></i></div>
-            </div>
-        </div>
-        <div class="col-sm-4 text-center">
-            <div class="row">
-                <div class="col-sm-10 text-center"><h3>Clean</h3><p>In the first 30 seconds of a user's visit to your site they decide if they're going to stay..</p><i class="fa fa-mobile fa-5x"></i></div>
-            </div>
-        </div>
-    </div><!--/row-->
     <div class="row"><br></div>
-</div><!--/container-->
+    <div class="row">
+        <div class="col-md-4 text-center">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1 text-center"><h3>Template markup languages</h3><p>HTML + a bit of node.js template engines</p><i class="fa fa-database fa-5x"></i></div>
+            </div>
+        </div>
+        <div class="col-md-4 text-center">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1 text-center"><h3>CSS</h3><p> + CSS3 with Compass/Grunt usage</p><i class="fa fa-database fa-5x"></i></div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1 text-center"><h3>PHP</h3><p>... the most experience I have with + using a Composer dependency manager</p><i class="fa fa-desktop fa-5x"></i></div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1 text-center"><h3>SQL + NoSQL</h3><p>The most I have worked with is MySQL and PostgreSQL, but I also touched the skin of NoSQL (MongoDB)</p><i class="fa fa-desktop fa-5x"></i></div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1 text-center"><h3>JavaScript</h3><p>Raw JavaScript knowledge + using jQuery libraries and it's plugins. Love to use AJAX technology.</p><i class="fa fa-desktop fa-5x"></i></div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1 text-center"><h3>C#</h3><p>I created some desktop applications with it and also one ASP.NET web page.</p><i class="fa fa-desktop fa-5x"></i></div>
+            </div>
+        </div>
+    </div>
+    <div class="row"><br></div>
+</div>
