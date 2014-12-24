d8041c3726915857852022a6083798755c56c8c9
Saso Sabotin <sasos90@gmail.com>
2014-12-24 15:57:15 +0100

    Second section ready

--- application/views/my_controller/index/sections/section3.php
+++ application/views/my_controller/index/sections/section3.php
@@ -1,21 +1,17 @@
-<h1 class="text-center v-center">Sectionalize3.</h1>
+<h1 class="text-center v-center">... what I do?</h1>
 <div class="container">
-    <div class="row">
-        <div class="col-sm-4">
-            <div class="row">
-                <div class="col-sm-10 col-sm-offset-2 text-center"><h3>Robust</h3><p>There is other content and snippets of details or features that can be placed here..</p><i class="fa fa-cog fa-5x"></i></div>
-            </div>
-        </div>
-        <div class="col-sm-4 text-center">
-            <div class="row">
-                <div class="col-sm-10 col-sm-offset-1 text-center"><h3>Simple</h3><p>You may also want to create content that compells users to scroll down more..</p><i class="fa fa-user fa-5x"></i></div>
-            </div>
-        </div>
-        <div class="col-sm-4 text-center">
-            <div class="row">
-                <div class="col-sm-10 text-center"><h3>Clean</h3><p>In the first 30 seconds of a user's visit to your site they decide if they're going to stay..</p><i class="fa fa-mobile fa-5x"></i></div>
-            </div>
-        </div>
-    </div><!--/row-->
     <div class="row"><br></div>
-</div><!--/container-->
+    <div class="row">
+        <div class="col-md-6">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1 text-center"><h3>Web pages</h3><p>This section is my highest priority in the world of programming. That's a thing I want to work at the highest level</p><i class="fa fa-desktop fa-5x"></i></div>
+            </div>
+        </div>
+        <div class="col-md-6 text-center">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1 text-center"><h3>Desktop applications</h3><p>I also love to create some simple and small desktop applications </p><i class="fa fa-database fa-5x"></i></div>
+            </div>
+        </div>
+    </div>
+    <div class="row"><br></div>
+</div>
