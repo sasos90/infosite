6c11665078fa88d672b1f4242a40c101cd40ca33
Saso Sabotin <sasos90@gmail.com>
2014-12-25 10:37:52 +0100

    Section 3 modified

--- application/views/my_controller/index/sections/section3.php
+++ application/views/my_controller/index/sections/section3.php
@@ -4,12 +4,20 @@
     <div class="row">
         <div class="col-md-6">
             <div class="row">
-                <div class="col-md-10 col-md-offset-1 text-center"><h3>Web pages</h3><p>This section is my highest priority in the world of programming. That's a thing I want to work at the highest level</p><i class="fa fa-desktop fa-5x"></i></div>
+                <div class="col-md-10 col-md-offset-1 text-center">
+                    <h3>Web pages</h3>
+                    <p>This section is my highest priority in the world of programming. That's a thing I want to work at the highest level</p>
+                    <i class="fa fa-tablet fa-5x"></i>
+                </div>
             </div>
         </div>
         <div class="col-md-6 text-center">
             <div class="row">
-                <div class="col-md-10 col-md-offset-1 text-center"><h3>Desktop applications</h3><p>I also love to create some simple and small desktop applications </p><i class="fa fa-database fa-5x"></i></div>
+                <div class="col-md-10 col-md-offset-1 text-center">
+                    <h3>Desktop applications</h3>
+                    <p>I also love to create some simple and small desktop applications </p>
+                    <i class="fa fa-desktop fa-5x"></i>
+                </div>
             </div>
         </div>
     </div>
