ed9352feee0c8eb4222659d633fc28b93fbcf579
Saso Sabotin <sasos90@gmail.com>
2015-01-10 01:01:27 +0100

    Removed preloaded views in the index page

--- application/views/main/page.php
+++ application/views/main/page.php
@@ -1,18 +1,8 @@
 <section class="container-fluid" id="section1">
     <?php $this->load->view("my_controller/index/sections/section1"); ?>
 </section>
-<section class="container-fluid" id="section2">
-    <?php $this->load->view("my_controller/index/sections/section2"); ?>
-</section>
-<section class="container-fluid" id="section3">
-    <?php $this->load->view("my_controller/index/sections/section3"); ?>
-</section>
-<section class="container-fluid" id="section4">
-    <?php $this->load->view("my_controller/index/sections/section4"); ?>
-</section>
-<section class="container-fluid" id="section5">
-    <?php $this->load->view("my_controller/index/sections/section5"); ?>
-</section>
-<section class="container-fluid" id="section6">
-    <?php $this->load->view("my_controller/index/sections/section6"); ?>
-</section>
+<section class="container-fluid" id="section2"></section>
+<section class="container-fluid" id="section3"></section>
+<section class="container-fluid" id="section4"></section>
+<section class="container-fluid" id="section5"></section>
+<section class="container-fluid" id="section6"></section>
